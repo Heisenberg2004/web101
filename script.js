5af39c77aee89ca6b48f58ce82ae6249c0e75d9d
WASUPON TIYAKOM <wave@MacBook-Pro-khxng-WASUPON.local>
2025-11-24 11:36:18 +0700

    Update button style and JS alert message

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const showMessage = () => {
   alert('Hello! You clicked the button.')
 }
+
+const heading = document.querySelector('h1')
+
+function changeTitle() {
+  heading.textContent = 'You changed the title!'
+}
